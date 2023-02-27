b15f87d07baeeaf6c0fcfde5b2a80292bb3c713f
GSUS100 <gca.2000@hotmail.com>
2023-02-27 13:06:54 -0500

    Updated Enrollment.java
    I used basically the same structure from roster with the grow method so it increases by blocks of 4

--- Enrollment.java
+++ Enrollment.java
@@ -2,39 +2,68 @@ public class Enrollment {
     private EnrollStudent[] enrollStudents;
     private int size;
 
-    public Enrollment() {
-        enrollStudents = new EnrollStudent[100];
+    public Enrollment(int size)
+    {
+        enrollStudents = new EnrollStudent[4];
         size = 0;
     }
 
-    public void add(EnrollStudent enrollStudent) {
-        enrollStudents[size] = enrollStudent;
-        size++;
+
+
+    public void add(EnrollStudent enrollStudent) //add to the end of array
+    {
+        if (size == enrollStudents.length) {
+            grow();
+        }
+        enrollStudents[size++] = enrollStudent;
     }
 
-    public void remove(EnrollStudent enrollStudent) {
-        for (int i = 0; i < size; i++) {
-            if (enrollStudents[i].equals(enrollStudent)) {
-                enrollStudents[i] = enrollStudents[size - 1];
-                enrollStudents[size - 1] = null;
-                size--;
-                break;
+    private void grow() {
+        EnrollStudent[] newEnrollStudents = new EnrollStudent[enrollStudents.length + 4];
+        System.arraycopy(enrollStudents, 0, newEnrollStudents, 0, size);
+        enrollStudents = newEnrollStudents;
+    }
+
+
+    //move the last one in the array to replace the deleting index position
+    public void remove(EnrollStudent enrollStudent){
+        int index = find(enrollStudent);
+        if (index == -1) {
+            System.out.println("Student not found in EnrolledStudents()")
+        }
+        else {
+            for (int i = index; i < size - 1; i++) {
+                enrollStudents[i] = enrollStudents[i + 1];
             }
+            size--;
         }
     }
 
-    public boolean contains(EnrollStudent enrollStudent) {
+    public int find(EnrollStudent student)
+    {
         for (int i = 0; i < size; i++) {
-            if (enrollStudents[i].equals(enrollStudent)) {
-                return true;
+            if (enrollStudents[i].equals(student)) {
+                return i;
             }
         }
-        return false;
+        return -1;
     }
+
+    public boolean contains(EnrollStudent enrollStudent){
+        return find(enrollStudent) != -1;
+    }
+
 
     public void print() {
-        for (int i = 0; i < size; i++) {
-            System.out.println(enrollStudents[i]);
+        if(size == 0){
+            System.out.println("Enrolled Students is empty!");
+        } else {
+            for (int i = 0; i < size; i++) {
+                System.out.println(enrollStudents[i].toString());
+                //System.out.println(roster[i].getProfile() + " (" + roster[i].getMajor().getCode()+" "+ roster[i].getMajor()+" "+roster[i].getMajor().getSchool()+ ") credits completed: " + roster[i].getCreditCompleted() + " ("+ roster[i].getStanding()+")");
+            }
         }
-    }
+        System.out.println("* end of enrolled students **");
+    } //print the array as is without sorting
+
 }
